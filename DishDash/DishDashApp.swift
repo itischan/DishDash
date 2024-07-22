ef09b0ecc59d590003524cbdb8a8c67060b35c54
itischan <chandrukumaran37@gmail.com>
2024-07-22 10:50:59 -0400

    Initial Commit

---
+++ DishDash/DishDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishDashApp.swift
+//  DishDash
+//
+//  Created by ck on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct DishDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
