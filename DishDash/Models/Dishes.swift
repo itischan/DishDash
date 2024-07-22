3811dc77286857c42b8ab8af5e621ee89e88407b
itischan <chandrukumaran37@gmail.com>
2024-07-22 11:16:37 -0400

    [Feature] Added a data model using a api from sean allen course

---
+++ DishDash/Models/Dishes.swift
@@ -0,0 +1,64 @@
+//
+//  Dishes.swift
+//  DishDash
+//
+//  Created by ck on 2024-07-22.
+//
+
+import Foundation
+
+
+struct Dishes: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+
+struct MockData {
+    
+    static let sampleAppetizer = Dishes(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let dishes       = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+    static let orderItemOne     = Dishes(id: 0001,
+                                           name: "Test Appetizer One",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItemTwo     = Dishes(id: 0002,
+                                           name: "Test Appetizer Two",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItemThree   = Dishes(id: 0003,
+                                           name: "Test Appetizer Three",
+                                           description: "This is the description for my appetizer. It's yummy.",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 99)
+    
+    static let orderItems       = [orderItemOne, orderItemTwo, orderItemThree]
+}
---
+++ DishDash/ViewModels/DishViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DishViewModel.swift
+//  DishDash
+//
+//  Created by ck on 2024-07-22.
+//
+
+import Foundation
+
+
+class DishViewModel:ObservableObject{
+    
+}
