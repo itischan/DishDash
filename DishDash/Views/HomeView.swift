61ee9a446c1f29f5d29f2438296cfa51c4ebc2af
itischan <chandrukumaran37@gmail.com>
2024-07-23 10:43:10 -0400

    [Feature] added a image for each item in dish from imageUrl

--- DishDash/Views/HomeView.swift
+++ DishDash/Views/HomeView.swift
@@ -13,18 +13,7 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             List(dishes) { dish in
-                VStack {
-                    HStack(spacing: 20) {
-                        Image("asian-flank-steak")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 100)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                        Text(dish.name)
-                            .font(.headline)
-                            .frame(maxWidth: .infinity)
-                    }
-                }
+               layout(selectedDish: dish)
             }
             .onAppear {
                 Task {
@@ -48,4 +37,35 @@ struct HomeView: View {
 
 #Preview {
     HomeView()
+}
+
+
+extension HomeView{
+    private func layout(selectedDish dish:Dishes)->some View{
+        VStack {
+            HStack(spacing: 20) {
+                if let imageURL = URL(string: dish.imageURL) {
+                            AsyncImage(url: imageURL) { image in
+                                image.resizable()
+                                    .scaledToFill()
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(.buttonBorder)
+                            } placeholder: {
+                                ProgressView() // Show an empty view while the image is loading
+                            }
+                        } else {
+                           ProgressView() // Show an empty view if the image URL is invalid
+                    }
+                
+//                Image("asian-flank-steak")
+//                    .resizable()
+//                    .scaledToFill()
+//                    .frame(width: 100, height: 100)
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                Text(dish.name)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+    }
 }
